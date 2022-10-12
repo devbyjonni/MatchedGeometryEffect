0f43086b279fb78b7ef90ee9127b8764208c3d55
Jonni Åkesson <enterthesource@me.com>
2022-10-12 17:26:36 +0200

    Initial Commit

---
+++ MatchedGeometryEffect/MatchedGeometryEffectApp.swift
@@ -0,0 +1,17 @@
+//
+//  MatchedGeometryEffectApp.swift
+//  MatchedGeometryEffect
+//
+//  Created by Jonni Akesson on 2022-10-12.
+//
+
+import SwiftUI
+
+@main
+struct MatchedGeometryEffectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
